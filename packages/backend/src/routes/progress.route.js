f8f31bd4e5d15693f842802ae76c9f0a2ae76837
meer-45 <shaikmeer45@gmail.com>
2026-03-14 14:38:15 +0530

    added progress route

---
+++ packages/backend/src/routes/progress.route.js
@@ -0,0 +1,15 @@
+import { Router } from "express"
+import {
+  getProgressController,
+  updateProgressController,
+  resetWeeklyProgressController
+} from "../controllers/progress.controller.js"
+import { verifyJWT } from "../middlewares/auth.middlewares.js"
+
+const router = Router()
+
+router.get("/", verifyJWT, getProgressController)
+router.patch("/update", verifyJWT, updateProgressController)
+router.patch("/reset-weekly", verifyJWT, resetWeeklyProgressController)
+
+export default router
